2e861f0ed4a21041aab8438bb39d75ed7f223679
VitalisOscar <vitalisoscar25@gmail.com>
2021-09-20 18:15:59 +0300

    home area

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -8,66 +8,81 @@
 
 @section('body')
 
-<header class="py-3 bg-white sticky-top">
+<style>
+    body{
+        background: #fafafa !important;
+    }
+</style>
+
+<header class="py-3 bg-dark sticky-top">
     <nav class="navbar py-0">
-        <div class="container">
+        <div class="container d-block d-sm-flex">
 
-            <a href="" class="navbar-brand">{{ config('app.name') }}</a>
+            <div class="d-flex align-items-center">
+                <a href="{{ route('home') }}" class="navbar-brand mr-auto">
+                    <img style="height: 50px" src="{{ asset('img/alphawhite.svg') }}" alt="{{ config('app.name') }}">
+                </a>
 
-            <div class="float-right d-flex align-items-center">
-                <div class="links">
+                <button class="navbar-toggler border d-sm-none" onclick="$('#menu').toggleClass('d-none'); $('.navbar-toggler i').toggleClass('fa-bars fa-times');">
+                    <i class="fa fa-bars text-white"></i>
+                </button>
+            </div>
+
+            <div class="py-3 py-sm-0 float-sm-right d-none d-sm-flex align-items-center" id="menu">
+                <div class="links pt-3 pb-2 pt-sm-0 pb-sm-0">
                     <a href="{{ route('web.auth.login') }}" class="active">Home</a>
                     <a href="{{ route('web.auth.login') }}">Log in</a>
                 </div>
 
-                <a class="btn btn-success shadow-none btn-rounded" href="{{ route('web.auth.signup') }}">Get Started</a>
+                <a class="btn btn-outline-white shadow-none btn-rounded cfa-btn" href="{{ route('web.auth.signup') }}">Get Started</a>
             </div>
         </div>
     </nav>
 </header>
 
-<section class="section-shaped bg">
+<section class="section-shaped" style="">
+
+    <div class="shape shape-style-2" style="background: linear-gradient(225deg, #87ceeb, #87ceeb95, #0000, #0000, #0000, #87ceeb20)">
+        <span>
+            <svg class="clouds cloud1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="128" height="128" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+                <path fill="#eee" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+            </svg>
+        </span>
+
+        <span>
+            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="192" height="192" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+                <path fill="#eee" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+            </svg>
+        </span>
+
+        <span>
+            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="192" height="192" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+                <path fill="#eee" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+            </svg>
+        </span>
+
+        <span>
+            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="128" height="128" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+                <path fill="#eee" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+            </svg>
+        </span>
+
+        <span>
+            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="128" height="128" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+                <path fill="#eee" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
+            </svg>
+        </span>
+    </div>
 
     <div class="hero-content">
         <div class="container">
 
             <div class="row">
+
                 <div class="col-lg-6 d-flex align-items-center">
 
-                    <div class="shape shape-style-1">
-                        <span>
-                            <svg class="clouds cloud1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="128" height="128" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                                <path fill="#ddd" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
-                            </svg>
-                        </span>
-
-                        <span>
-                            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="192" height="192" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                                <path fill="#ddd" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
-                            </svg>
-                        </span>
-
-                        <span>
-                            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="192" height="192" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                                <path fill="#ddd" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
-                            </svg>
-                        </span>
-
-                        <span>
-                            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="128" height="128" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                                <path fill="#ddd" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
-                            </svg>
-                        </span>
-
-                        <span>
-                            <svg class="clouds cloud2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0" y="0" width="128" height="128" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
-                                <path fill="#ddd" id="cloud-icon" d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"/>
-                            </svg>
-                        </span>
-                     </div>
-
                      <div>
-                        <h1 class="hero-title">Simple Digital Outdoor Advertising</h1>
+                        <h1 class="hero-title"><span class="text-primary">Simple</span> Digital Outdoor <span class="text-dar">Advertising</span></h1>
 
                         <p class="lead">
                             {{ config('app.name') }} has been built to enable your business and products gain more visibility through digital outdoor advertising.
@@ -76,11 +91,12 @@
 
                         <div>
                             <a href="#about" class="mb-3 btn btn-lg btn-outline-primary shadow-none">Learn More</a>
-                            <a href="{{ route('web.auth.signup') }}" class="mb-3 btn btn-lg btn-success shadow-none">Get Started</a>
+                            <a href="{{ route('web.auth.signup') }}" class="mb-3 btn btn-lg btn-default shadow-none">Get Started</a>
                         </div>
                      </div>
 
                 </div>
+
                 <div class="col-lg-6">
                     <img src="{{ asset('img/hero_vector.png') }}" class="img-fluid">
                 </div>
@@ -120,15 +136,10 @@
 
 
 <section id="hiw" class="py-5 section-shaped hiw">
-    <div class="shape shape-light bg-gradient-success shape-style-1">
+    <div class="shape shape-light bg-default shape-style-1">
         <span class="span-200"></span>
         <span class="span-100"></span>
         <span class="span-150"></span>
-        <span class="span-75"></span>
-        <span class="span-100"></span>
-        <span class="span-100"></span>
-        <span class="span-100"></span>
-        <span class="span-100"></span>
     </div>
 
 
@@ -142,7 +153,7 @@
 
         <div class="row">
 
-            <div class="col-lg-6 hiw-item mb-4">
+            <div class="col-lg-6 hiw-item mb-5">
                 <div class="d-sm-flex align-items-cente">
 
                     <span class="hiw-circle">1</span>
@@ -150,13 +161,13 @@
                     <div class="ml-sm-4">
                         <h4 class="mb-2 text-white"><strong>Create an Account</strong></h4>
 
-                        <p class="text-white lead my-0">Create a free account with your personal and business info, or log in if you are already registered</p>
+                        <p class="text-white my-0">Create a free account with your personal and business info, or log in if you are already registered</p>
                     </div>
                 </div>
             </div>
 
 
-            <div class="col-lg-6 hiw-item mb-4">
+            <div class="col-lg-6 hiw-item mb-5">
                 <div class="d-sm-flex align-items-cente">
 
                     <span class="hiw-circle">2</span>
@@ -164,7 +175,7 @@
                     <div class="ml-sm-4">
                         <h4 class="mb-2 text-white"><strong>Submit your Ads</strong></h4>
 
-                        <p class="text-white lead my-0">Creating an ad will only take you a few minutes. Once you complete payment, your ad will be submitted immediately</p>
+                        <p class="text-white my-0">Creating an ad will only take you a few minutes. Once approved, you can complete payment and wait for airing</p>
                     </div>
                 </div>
             </div>
@@ -177,7 +188,7 @@
                     <div class="ml-sm-4">
                         <h4 class="mb-2 text-white"><strong>Increase your visibility</strong></h4>
 
-                        <p class="text-white lead my-0">Once your advert is received and approved by us, it will be advertised on our screens to hundreds of thousands of potential clients</p>
+                        <p class="text-white my-0">Once your advert is received and approved by us, it will be advertised on our screens to hundreds of thousands of potential clients</p>
                     </div>
                 </div>
             </div>
@@ -210,7 +221,7 @@
                     </p>
 
                     <div class="position-absolute bottom-0 text-center left-0 right-0 pb-5">
-                        <a href="{{ route('web.auth.signup') }}" class="btn btn-outline-success shadow-none btn-lg px-4 btn-rounded">Get Started</a>
+                        <a href="{{ route('web.auth.signup') }}" class="btn btn-outline-default shadow-none btn-lg px-4 btn-rounded">Get Started</a>
                     </div>
                 </div>
             </div>
@@ -230,7 +241,7 @@
                     </p>
 
                     <div class="position-absolute bottom-0 text-center left-0 right-0 pb-5">
-                        <a href="{{ route('web.auth.signup') }}" class="btn btn-outline-success shadow-none btn-lg px-4 btn-rounded">Get Started</a>
+                        <a href="{{ route('web.auth.signup') }}" class="btn btn-outline-default shadow-none btn-lg px-4 btn-rounded">Get Started</a>
                     </div>
                 </div>
             </div>
@@ -246,11 +257,11 @@
                     <h4 class="text-center"><strong>Trusted by Many</strong></h4>
 
                     <p class="lead text-justify">
-                        We are a trusted, leading outdoor advertising company in the region, with thousands of other businesses already using our other services
+                        We are a trusted, leading digital advertising company, with a presence in multiple countries and many businesses using our services
                     </p>
 
                     <div class="position-absolute bottom-0 text-center left-0 right-0 pb-5">
-                        <a href="{{ route('web.auth.signup') }}" class="btn btn-outline-success shadow-none btn-lg px-4 btn-rounded">Get Started</a>
+                        <a href="{{ route('web.auth.signup') }}" class="btn btn-outline-default shadow-none btn-lg px-4 btn-rounded">Get Started</a>
                     </div>
                 </div>
             </div>
@@ -259,7 +270,7 @@
     </div>
 </section>
 
-<section class="py-5 section-shaped">
+<section class="py-5 section-shaped d-none">
     <div class="shape shape-light bg-gradient-success shape-style-1">
         <span class="span-200"></span>
         <span class="span-100"></span>
@@ -296,22 +307,17 @@
     </div>
 </section>
 
-<section class="py-5 bg-white">
-    <div class="container">
-
-        <div class="text-center">
-            <img src="{{ asset('img/magnate_logo.png') }}" class="d-block mx-auto mb-3" style="height: 100px" alt="Magnate Logo">
-
-            <div class="">
-                <p class="lead mt-0 mb-3">{{ config('app.name') }} is powered by <a href="https://magnate-ventures.com" target="_blank">Magnate Ventures Limited</a></p>
-                <div class="">
-                    <a href="http://www.linkedin.com/company/magnate-ventures-ltd" target="_blank" class="btn btn-facebook px-3 shadow-none" title="LinkedIn"><i class="fa fa-linkedin" style="font-size: 1.4em"></i></a>
-                    <a href="http://twitter.com/magnateventures" target="_blank" class="btn btn-twitter px-3 shadow-none" title="Twitter"><i class="fa fa-twitter" style="font-size: 1.4em"></i></a>
-                </div>
-            </div>
+<footer class="py-3" style="background: #f2f5f8">
+    <div class="container d-sm-flex align-items-sm-center">
+        <div class="mb-3 mb-sm-0">
+            {{ config('app.name') }} is powered by <a href="https://alphaandjam.net" target="_blank">Alpha And Jam Limited</a>
         </div>
 
+        <div class="ml-sm-auto">
+            <a href="https://linkedin.com/company/" target="_blank" class="btn btn-facebook px-2 py-1 shadow-none" title="LinkedIn"><i class="fa fa-linkedin" style="font-size: 1.4em"></i></a>
+            <a href="https://facebook.com" target="_blank" class="btn btn-facebook px-2 py-1 shadow-none" title="Facebook"><i class="fa fa-facebook" style="font-size: 1.4em"></i></a>
+        </div>
     </div>
-</section>
+</footer>
 
 @endsection
