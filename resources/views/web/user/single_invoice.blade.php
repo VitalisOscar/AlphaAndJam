0e7661f6006aae8f89011de55d07ad451d80cfc8
VitalisOscar <vitalisoscar25@gmail.com>
2021-10-09 21:59:18 +0300

    Fix pesapal link

--- resources/views/web/user/single_invoice.blade.php
+++ resources/views/web/user/single_invoice.blade.php
@@ -38,7 +38,7 @@
             <h4 class="font-weight-600">Actions</h4>
             <a href="{{ route('web.user.invoices.single.download', $invoice->number) }}" class="btn btn-default shadow-none mb-3 btn-block"><i class="fa fa-print mr-1"></i>Download/Print</a>
             @if($invoice->isUnpaid())
-            <form action="{{ config('web.pesapal.make', $invoice->number) }}" method="get" class="d-block mb-3">
+            <form action="{{ route('web.pesapal.make', $invoice->number) }}" method="get" class="d-block mb-3">
                 <button class="btn btn-primary shadow-none btn-block"><i class="fa fa-credit-card mr-1"></i>Pay Online</button>
             </form>
             {{-- <button class="btn btn-primary shadow-none btn-block" data-toggle="modal" data-target="#payment_instructions"><i class="fa fa-credit-card mr-1"></i>Payment</button> --}}
